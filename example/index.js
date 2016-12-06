8b56a5d35f4197bfa45c20e3644cc9187bea0870
Ionică Bizău <bizauionica@gmail.com>
2016-12-06 18:27:57 +0200

    Added example

--- example/index.js
+++ example/index.js
@@ -1,5 +1,13 @@
 "use strict";
 
-const rucksack = require("../lib");
+const Rucksack = require("..");
 
-console.log(rucksack());
+
+let bundler = new Rucksack();
+
+// Add remote url as resource
+//bundler.add("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js");
+bundler.add(`${__dirname}/data/main.js`);
+bundler.add(`${__dirname}/data/another-main.js`);
+bundler.add(`${__dirname}/data/main.css`);
+bundler.bundle();
