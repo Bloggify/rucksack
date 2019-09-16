128b851ae6f679c3a03c34b93f6000e7de468ba3
Ionică Bizău <bizauionica@gmail.com>
2019-09-16 10:25:03 -0300

    upgrade postcss

--- example/index.js
+++ example/index.js
@@ -14,7 +14,7 @@ bundler.add(`${__dirname}/data/bar.css`)
 bundler.add(`${__dirname}/data/main.css`)
 bundler.add("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css")
 
-//bundler.bundleCSS()
+bundler.bundleCSS()
 // => info  [Tuesday, November 28, 2017 06:53:48 AM] Bundling  the styles.
 // => warn  [Tuesday, November 28, 2017 06:53:48 AM] Skipping remote @import of "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css" as resource is not allowed.
 // => @import url(https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css);strong{color:#000}strong{color:#ff0}body{background:#fff;-webkit-transform:translate(10px);transform:translate(10px)}
