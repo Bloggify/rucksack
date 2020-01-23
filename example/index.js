fc901c22075533ba684762419126e56d8cea810a
Ionică Bizău <bizauionica@gmail.com>
2020-01-23 18:01:39 -0500

    Update the dependencies

--- example/index.js
+++ example/index.js
@@ -12,13 +12,17 @@ bundler.add(`${__dirname}/data/main.js`)
 bundler.add(`${__dirname}/data/another-main.js`)
 bundler.add(`${__dirname}/data/bar.css`)
 bundler.add(`${__dirname}/data/main.css`)
-bundler.add("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css")
+bundler.add({
+    type: "css",
+    url: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css",
+    inline: true
+})
 
-bundler.bundleCSS()
+bundler.bundleCSS("test.css")
 // => info  [Tuesday, November 28, 2017 06:53:48 AM] Bundling  the styles.
 // => warn  [Tuesday, November 28, 2017 06:53:48 AM] Skipping remote @import of "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css" as resource is not allowed.
 // => @import url(https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css);strong{color:#000}strong{color:#ff0}body{background:#fff;-webkit-transform:translate(10px);transform:translate(10px)}
 
-bundler.bundleJS()
+bundler.bundleJS("test.js")
 // => info  [Tuesday, November 28, 2017 06:53:48 AM] Bundling  the scripts.
 // => (function e(t,n,r){...})
