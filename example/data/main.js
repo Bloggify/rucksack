2cb4bcd0c3470ad6c7cd69e1a47ab253ef694909
Ionică Bizău <bizauionica@gmail.com>
2020-09-13 07:02:08 +0300

    Add support for await-async

--- example/data/main.js
+++ example/data/main.js
@@ -4,5 +4,15 @@ const math = require("./math")
     ;
 
 console.log(math.square(2, 4));
-console.log($("#my-id"));
+//console.log($("#my-id"));
 console.log(camelo("hey there"));
+
+const sleep = async ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+    console.log(1)
+    await sleep(420)
+    console.log(2)
+})()
