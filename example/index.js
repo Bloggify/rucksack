f729636a3cb6ef96598406e5fb83099939088360
Ionică Bizău <bizauionica@gmail.com>
2022-02-23 13:35:36 +0100

    Compile css

--- example/index.js
+++ example/index.js
@@ -11,18 +11,18 @@ let bundler = new Rucksack({
 })
 
 // Add remote url as resource
-//bundler.add("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js")
+bundler.add("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js")
 bundler.add(`${__dirname}/data/main.js`)
 bundler.add(`${__dirname}/data/another-main.js`)
-//bundler.add(`${__dirname}/data/bar.css`)
-//bundler.add(`${__dirname}/data/main.css`)
+bundler.add(`${__dirname}/data/bar.css`)
+bundler.add(`${__dirname}/data/main.css`)
 //bundler.add({
 //    type: "css",
 //    url: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css",
 //    inline: true
 //})
-//
-//bundler.bundleCSS(`${__dirname}/test.css`)
+
+bundler.bundleCSS(`${__dirname}/test.css`)
 // => info  [Tuesday, November 28, 2017 06:53:48 AM] Bundling  the styles.
 // => warn  [Tuesday, November 28, 2017 06:53:48 AM] Skipping remote @import of "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css" as resource is not allowed.
 // => @import url(https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css);strong{color:#000}strong{color:#ff0}body{background:#fff;-webkit-transform:translate(10px);transform:translate(10px)}
